4b410477c5dd10b9756d9e586bfa3ed4503e744c
Tom Bartindale <tom.bartindale@monash.edu>
2021-11-26 13:43:09 +1100

    slide update history logger

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,28 +1,32 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 admin.initializeApp();
 const db = admin.database();
 
 exports.onSlideInputMeeting = functions.database
-  .ref("/data/slides/{meetingid}/current/{field}")
+  .ref('/data/slides/{meetingid}/current/{field}')
   .onWrite(async (change, context) => {
     // functions.logger.log(change.after);
     const timestamp = Date.now();
     await db
-      .ref(
-        `/data/slides/${context.params.meetingid}/_history/${timestamp}/${context.params.field}`
-      )
+      .ref(`/data/slides/${context.params.meetingid}/_history/${timestamp}/${context.params.field}`)
       .set(change.after.val());
   });
 
 exports.onSlideInputSequence = functions.database
-  .ref("/sequence/{sequenceid}/slides/current/{field}")
+  .ref('/sequence/{sequenceid}/slides/current/{field}')
   .onWrite(async (change, context) => {
     // functions.logger.log(change.after);
     const timestamp = Date.now();
     await db
-      .ref(
-        `/sequence/${context.params.sequenceid}/_history/${timestamp}/${context.params.field}`
-      )
+      .ref(`/sequence/${context.params.sequenceid}/_history/${timestamp}/${context.params.field}`)
       .set(change.after.val());
+  });
+
+exports.onSlideChanged = functions.database
+  .ref('/config/{meetingid}/current/currentState/plugins/slides/currentslide')
+  .onWrite(async (change, context) => {
+    // functions.logger.log(change.after);
+    const timestamp = Date.now();
+    await db.ref(`/data/slides/${context.params.meetingid}/_history/${timestamp}/_slide`).set(change.after.val());
   });
